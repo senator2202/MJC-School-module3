b2e9fcbb5485aca104ec13a5872e7d504f09253c
Alexey Kharitonov <senator220291@gmail.com>
2020-12-23 16:36:29 +0300

    add user and order entities

---
+++ src/main/java/com/epam/esm/model/entity/Order.java
@@ -0,0 +1,40 @@
+package com.epam.esm.model.entity;
+
+public class Order {
+    private Long id;
+    private User user;
+    private GiftCertificate giftCertificate;
+    private String orderDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public GiftCertificate getGiftCertificate() {
+        return giftCertificate;
+    }
+
+    public void setGiftCertificate(GiftCertificate giftCertificate) {
+        this.giftCertificate = giftCertificate;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+}
